b1b8f20dae5d05d2e68e77ab55649a5cb990a135
nnfsth <hja5432@gmail.com>
2017-10-07 23:18:31 +0900

    cd pwd coding

--- src/commands.c
+++ src/commands.c
@@ -1,11 +1,13 @@
 #include <string.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+  chdir(argv[1]);
+  
   // TODO: Fill it!
   return 0;
 }
@@ -13,7 +15,9 @@ int do_cd(int argc, char** argv) {
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+  char *now= (char*)malloc(sizeof(char)*80);
+  getcwd(now,1024);
+  printf("%s\n",now);
   // TODO: Fill it!
 
   return 0;
@@ -21,10 +25,16 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc==2&&!strcmp(argv[0],"cd"))
+  	return 1;
+  else
+	return 0;
 }
-
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc==1&&!strcmp(argv[0],"pwd")){
+  	return 1;
+}
+  else 
+	return 0;
 }
