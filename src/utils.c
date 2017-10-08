cd1cf7a0c39ba79bae3947c6a7995f4bfed0bfbf
nnfsth <hja5432@gmail.com>
2017-10-08 19:19:53 +0900

    parsing error fix

--- src/utils.c
+++ src/utils.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
 	char* temp=(char*)malloc(sizeof(char)*80);
-	char* tok;
+	char* tok=(char*)malloc(sizeof(char)*80);
 	char i=0;
 	*argc =0;
 	*argv = (char**)malloc(sizeof(char)*80);
@@ -15,20 +14,17 @@ void mysh_parse_command(const char* command,
 	int length=strlen(temp);
 	if(*(temp+length-1)=='\n')
 		*(temp+length-1)=0;
-	tok=strtok(temp," ");
+	tok=strtok(temp," \n\t");
 	while(tok!=NULL){
    		*(*argv+i)=(char*)malloc(sizeof(char)*80);
-		if(strcmp(tok,"\n")&&strcmp(tok,"\t")){
-			strcpy((*(*argv+i)),tok);
-			*argc+=1;
-			i++;
-		}
-		tok=strtok(NULL," ");
+		strcpy((*(*argv+i)),tok);
+		*argc+=1;
+		i++;
+		tok=strtok(NULL," \n\t");
 	}
 	if(*argc==0){
-		*argc+=1;
+		*argc=1;
+		**argv=(char*)malloc(sizeof(char)*80);
 		strcpy(**argv,"");
 	}
-		
-
 }
